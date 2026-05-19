8232ccc5bff7b07c1053f2f134eee9fdd35038a6
BorisITZaitsev <boryanzayzev@gmail.com>
2026-05-19 03:50:51 +0300

    README edited

--- FitServiceCRM/obj/Debug/net10.0/FitServiceCRM.AssemblyInfo.cs
+++ FitServiceCRM/obj/Debug/net10.0/FitServiceCRM.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Fitservicecrm")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("FitServiceCRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f430e78a68e53d50b5676c56650e6e9f84fe40c6")]
-[assembly: System.Reflection.AssemblyProductAttribute("Fitservicecrm")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Fitservicecrm")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe83a7783beb8466a51faca57d89feff79f8f297")]
+[assembly: System.Reflection.AssemblyProductAttribute("FitServiceCRM")]
+[assembly: System.Reflection.AssemblyTitleAttribute("FitServiceCRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
